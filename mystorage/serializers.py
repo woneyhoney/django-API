e1e99f09a4594a4ea7060d8e4ee28c91af6a13e5
woneyhoney <woneyhoney@ajou.ac.kr>
2019-10-31 00:54:49 +0900

    django api project

---
+++ mystorage/serializers.py
@@ -0,0 +1,28 @@
+from .models import Essay, Album, File
+from rest_framework import serializers
+
+class EssaySerializer(serializers.ModelSerializer):
+
+    author_name = serializers.ReadOnlyField(source = 'author.username')
+
+    class Meta:
+        model = Essay
+        fields = ('pk', 'title', 'body', 'author_name') 
+
+class AlbumSerializer(serializers.ModelSerializer):
+    
+    author_name = serializers.ReadOnlyField(source = 'author.username')
+    image = serializers.ImageField(use_url=True)
+
+    class Meta:
+        model = Album
+        fields = ('pk', 'author_name', 'image', 'desc')
+
+class FileSerializer(serializers.ModelSerializer):
+    
+    author_name = serializers.ReadOnlyField(source = 'author.username')
+    file = serializers.FileField(use_url=True)
+
+    class Meta:
+        model = File
+        fields = ('pk', 'author_name', 'file', 'desc')
